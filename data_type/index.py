e96a618aae584e5c353f0d7c8eaeae03ee1d951d
RAJ PATEL <rohitbeohari22@gmail.com>
2024-06-20 20:49:37 +0530

    third commit

---
+++ data_type/index.py
@@ -0,0 +1,13 @@
+# my_str = "rajrohit"
+# print(my_str.index('r',2 ))
+
+# print(my_str[0])
+# print(my_str[-1])
+
+# my_list=["raj",10,20,30,40,50,"patel"]
+# print(my_list.index(30))
+
+my_tuple=("rohit",20,30,70,90,"patel")
+print(my_tuple.index("patel"))
+
+
