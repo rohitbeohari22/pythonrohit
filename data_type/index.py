dbdd219f4f9f61a4b53602174492ac86aa9b423a
RAJ PATEL <rohitbeohari22@gmail.com>
2024-06-21 14:29:14 +0530

    third commit

--- data_type/index.py
+++ data_type/index.py
@@ -7,7 +7,8 @@
 # my_list=["raj",10,20,30,40,50,"patel"]
 # print(my_list.index(30))
 
-my_tuple=("rohit",20,30,70,90,"patel")
-print(my_tuple.index("patel"))
+# my_tuple=("rohit",20,30,70,90,"patel")
+# print(my_tuple.index("patel"))
+
 
 
---
+++ data_type/slice.py
@@ -0,0 +1,5 @@
+my_str = "ROHITRAJ"
+
+x= slice(1,3,1)
+print(my_str[x])
+
