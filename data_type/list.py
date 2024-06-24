7d26ced5469ec40e748f2550658548a6098e1069
RAJ PATEL <rohitbeohari22@gmail.com>
2024-06-24 14:32:31 +0530

    new commit

---
+++ data_type/list.py
@@ -0,0 +1,35 @@
+# my_list=[10,20,30,40,50,"abc",10]
+# print(my_list)
+# print(my_list[5])
+# print(my_list[0:4])
+# my_list[0]="Neeraj"
+# print(my_list)
+
+# list ke in-build function 
+# my_list=[10,20,30,40,50,80]
+# print(len(my_list))
+# print(max(my_list))
+# print(min(my_list))
+# print(sum(my_list))
+# print(list(my_list))
+
+my_list1=[7,8]
+my_list2=[55,66,10,6,9]
+# my_list2.append(50)  #last me add hota hai
+# print(my_list2)
+# my_list1.extend(my_list2)
+# print(my_list1)
+# my_list1.pop ()
+# print(my_list1)
+
+# my_list2.remove(30)
+# print(my_list2)
+
+# my_list1.insert(1,60)
+# print(my_list1)
+
+# my_list2.sort()
+# print(my_list2)
+
+my_list2.sort(reverse=True)
+print(my_list2)
