760c4a6c084a31695e3c6998873a7432cf8bddde
RAJ PATEL <rohitbeohari22@gmail.com>
2024-06-27 18:52:33 +0530

    my_new commit

---
+++ data_type/my_dict.py
@@ -0,0 +1,36 @@
+my_dict={"name": "raj", "age":25,"city":"bhopal" }
+# my_dict1={}
+# print(my_dict)
+# print(type(my_dict1))
+
+# my_dict['Name']="Raj"
+# print(my_dict)
+
+# my_dict['qulification'] = "M.tech"
+# print(my_dict)
+# my_dict['name']="pragya"
+# print(my_dict)
+
+# del my_dict['name']
+# print(my_dict)
+
+
+
+# my_dict.clear()
+# print(my_dict)
+
+# my_dict['name']
+
+# print(len(my_dict))
+# print(max(my_dict))
+
+# my_dict[1]="hello"
+# print(max(my_dict))
+
+# mutuble
+
+print(my_dict.keys())
+print(my_dict.values())
+print(my_dict.items)
+
+print(my_dict.get("qualification","None"))
