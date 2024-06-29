617b57b516795beb4930d94b2da8ca0e65eddfed
RAJ PATEL <rohitbeohari22@gmail.com>
2024-06-29 14:31:58 +0530

    frozen and set

--- data_type/set_py.py
+++ data_type/set_py.py
@@ -1,34 +1,102 @@
-my_set={10,30,60,20,40}
-print(my_set)
-
-print(len(my_set))
-print(max(my_set))
-print(min(my_set))
-
-my_set.add(50)
-print(my_set)
-
-my_list=[10,20,10,20,10,20]
-my_set.update(my_list)
-print(my_set)
-
-my_set.remove(30)
-print(my_set)
-
-my_set.discard(20)
-print(my_set)
-
-s1={10,20,30}
-s2={40,50,10}
-
-s3=s1.intersection(s2)
-print(s3)
-
-s4=s1.union(s2)
-print(s4)
 
 
+# my_set={10,30,60,20,40}
+# print(my_set)
+
+# print(len(my_set))
+# print(max(my_set))
+# print(min(my_set))
+
+# my_set.add(50)
+# print(my_set)
+
+# my_list=[10,20,10,20,10,20]
+# my_set.update(my_list)
+# print(my_set)
+
+# my_set.remove(30)
+# print(my_set)
+
+# my_set.discard(20)
+# print(my_set)
+
+# s1={10,20,30}
+# s2={40,50,10}
+
+# s3=s1.intersection(s2)
+# print(s3)
+
+# s4=s1.union(s2)
+# print(s4)
+
+# s5=s1.difference(s2)
+# print(s5)
 
 
-s5=s1.difference(s2)
-print(s5)
+# frozen set
+
+my_list=[2,4,6,"pragya",4,"raj",12,30,50]
+my_tuple=(5,8,"piku",4,88,9,10)
+my_set={7,6,9,5,7,"delhi"}
+my_dict={'name':'prgya','nickname':'raj'}
+
+print(frozenset[my_list])
+print(frozenset(my_tuple))
+print(frozenset(my_set))
+print(frozenset(my_dict))
+
+
+# in-built
+# my_tuple=(15,20,55,60,70)
+# my_list=[10,20,30,40,50]
+# print(frozenset[my_list])
+
+# print(max(my_list))
+# print(min(my_list))
+# print(sum(my_list))
+# print(len(my_list))
+# print(frozenset(my_list))
+
+# x=(frozenset(my_list))
+# y=(frozenset(my_tuple))
+
+# z=x.union(y)
+# print(z)
+
+# p=x.difference(y)
+# print(p)
+
+# r=x.intersection(y)
+# print(r)
+
+
+# import sys
+
+
+# my_list=[10,22,55,70,88]
+# my_tuple=(55,10,20,25,55)
+# my_set={10,20,30,40,50}
+
+
+# lsize=sys.getsizeof(my_list)
+# tsize=sys.getsizeof(my_tuple)
+# ssize=sys.getsizeof(my_set)
+# print(lsize)
+# print(tsize)
+# print(ssize)
+
+
+# import keyword
+# list=keyword.kwlist
+# print(list)
+
+
+
+
+
+
+
+
+
+
+
