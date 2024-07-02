8494b5b902beae894e2c3eef4e98ecf4b1f2311c
RAJ PATEL <rohitbeohari22@gmail.com>
2024-07-02 14:42:48 +0530

    loop.py

---
+++ conditional/loop.py
@@ -0,0 +1,18 @@
+# range findout object collection 
+# in-build function 
+
+# x=range(10)
+# print(x)
+# print(list(x))
+# print(tuple(x))
+# print(set(x))
+
+# x=range(-1,-11,-2)
+# x=range(-2,-11,-2)
+# print(list(x))
+
+# my_str=input("Enter your name: ")
+# total_ch=0
+# for i in my_str:
+#     total_ch=total_ch +1
+# print(total_ch)
