51a1ac898e62555b12a9c8db44286e8eab1dd96d
RAJ PATEL <rohitbeohari22@gmail.com>
2024-07-03 18:28:07 +0530

    whileloop

--- conditional/whileloop.py
+++ conditional/whileloop.py
@@ -1,14 +1,40 @@
-n=int(input("Enter how many returned no you want : "))
-i=1
-sum=0
-while i<=n:
-    # print(i)
-    sum=sum+i
-    if i<=n-1:
-     print(i,end="+")
-    else:
-     print(i,end=" = ")
-    i=i+1
-print(sum)
+# n=int(input("Enter how many returned no you want : "))
+# i=1
+# sum=0
+# while i<=n:
+#     # print(i)
+#     sum=sum+i
+#     if i<=n-1:
+#      print(i,end="+")
+#     else:
+#      print(i,end=" = ")
+#     i=i+1
+# print(sum)
     
    
+# n=int(input (" Enter no :"))
+# i=1
+# multi=1
+# while i<=n:
+#       multi=multi*i
+#       if i<n:
+#             print(i,end=" * ")
+#       else:
+#             print(i,end="=")  
+#       i=i+1
+# print(multi)   
+
+
+
+
+# n=int(input("enter no :")) 
+# i=1
+# multi=1
+# while i<n:
+#     multi=multi*n
+#     if i<n:
+#         print(n,end=" * ")
+#     else:
+#         print(n,end= " = ")  
+#     n=n-1
+# print(multi)          
