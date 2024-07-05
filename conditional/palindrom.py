13f4e0eab3deef99ba6db35257c0cc221be0847d
RAJ PATEL <rohitbeohari22@gmail.com>
2024-07-05 14:41:25 +0530

    palindrom

---
+++ conditional/palindrom.py
@@ -0,0 +1,82 @@
+# n=int(input(" Enter any value :"))
+# digit,x=0,n
+# while n>0:
+#     rev=n%10
+#     digit=digit*10+rev
+#     n=n//10
+
+# print(digit)    
+# print(n)
+# print(x)
+
+# if x==digit:
+#    print("palindrom")
+# else:
+#     print("not a palindrom ")   
+
+
+    # pattern
+
+    #   *
+    #   **
+    #   ***
+    #   ****
+    #   *****
+
+# code
+
+# n=int(input("How many row required :"))
+# i=1
+# while i<=n:
+#     print(i*'*' +(n-1)*(' '))
+#     i=i+1
+
+
+
+
+#     *
+#    **
+#   ***
+#  ****
+# *****
+
+
+# n=int(input("How many row required :"))
+# i=1
+# while i<=n:
+#     print((n-i)*(' ')+i*'*')
+#     i=i+1
+
+
+
+
+
+#      *
+#     * *
+#    * * *
+#   * * * *
+#  * * * * *
+
+
+# n=int(input("How many row required :"))
+# i=1
+# while i<=n:
+#     print((n-i)*(' ')+i*' *')
+#     i=i+1
+
+
+
+
+#      *
+#     ***
+#    *****
+#   *******
+#  *********
+# ***********
+
+
+# n=int(input("How many row required :"))
+# i=1
+# while i<=n:
+#     print((n-i)*(' ')+(2*i-1)*'*')
+#     i=i+1
