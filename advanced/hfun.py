329a3e334fecc298d3568f935749a8907d299f7e
RAJ PATEL <rohitbeohari22@gmail.com>
2024-07-16 14:30:37 +0530

    today commit

--- advanced/hfun.py
+++ advanced/hfun.py
@@ -10,23 +10,49 @@
 
 #WAP to find addtion of 2 list on the bases of indexing 
 
-my_list1=[1,2,3,4,5]
-my_list2=[5,4,3,2,1]
+# my_list1=[1,2,3,4,5]
+# my_list2=[5,4,3,2,1]
 
-def add(x,y):
-    return x+y
+# def add(x,y):
+#     return x+y
 
-new_list=list(map(add,my_list1,my_list2))
-print(new_list)
+# new_list=list(map(add,my_list1,my_list2))
+# print(new_list)
 
-def add(x,y):
-    return x+y
+# def add(x,y):
+#     return x+y
 
-# 
-new_list=list(map(add,(1,2,3,4),(1,2,3,4)))
-print(new_list)
+# # 
+# new_list=list(map(add,(1,2,3,4),(1,2,3,4)))
+# print(new_list)
 
 
-# 
-x=lambda a,b:a+b
-print((list(map(x,[1,2,3,4],[2,4,6,8]))))
+# #  
+# x=lambda a,b:a+b
+# print((list(map(x,[1,2,3,4],[2,4,6,8]))))
+
+#  0-9      asqi(48-57)
+#  A-Z          (65-90)
+#  a-z          (97-122)
+
+
+#  filter
+
+# my_list=[20,30,40,50,60]
+
+# def new(n):
+#     if n>=30:
+#         return True
+# print(list(filter(new,my_list)))    
+
+
+# reduce
+import functools
+my_list=[20,25,45,55]
+def smallest(x,y):
+    if x<y:
+        return x
+    else:
+        return y
+print(functools.reduce(smallest,my_list))   
+    
