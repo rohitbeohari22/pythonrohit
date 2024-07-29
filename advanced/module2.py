cf74fee3f2469f4b2f4dda72864cd48c71cb404c
RAJ PATEL <rohitbeohari22@gmail.com>
2024-07-29 14:26:01 +0530

    constructor made

--- advanced/module2.py
+++ advanced/module2.py
@@ -9,22 +9,64 @@
 # p=a(x,y)
 # print(p)
 
-#  
+#  decorator
 
-def outerfun(fun):
-    def innerfun(x,y):
-        x=x+10
-        y=y+10
+# def outerfun(fun):
+#     def innerfun(x,y):
+#         x=x+10
+#         y=y+10
 
-        return fun(x,y)
+#         return fun(x,y)
         
-        print("main function call")
-    return innerfun  
-@outerfun
-def main (x,y):
-    return x+y
-x=main(5,5)
-print(x)
+#         print("main function call")
+#     return innerfun  
+# @outerfun
+# def main (x,y):
+#     return x+y
+# x=main(5,5)
+# print(x)
+
+
+# def outerfun(fun):
+#     def innnerfun(x,y):
+#         x=x*10
+#         y=y*5
+#         return fun(x,y)
+#         print("main function call")
+#     return innnerfun
+# @outerfun
+# def main(x,y):
+#     return x+y
+# x=main(5,8)
+# print(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
        
 
 
---
+++ advanced/oops.py
@@ -0,0 +1,19 @@
+#  constructor 
+
+class Student:
+    x=10
+    y=20
+   
+    def __init__(self,x):
+        print(" gm ")
+              
+    def __init__(self,x,y):
+        print(" hello ")
+              
+    def __init__(self,x,y,z):
+        print("hi ")
+              
+    
+
+obj=Student(10,20,333)
+
